00bcebc8e2c31968546d0b24cb82ce3e1e14a2bf
TheCrether <denisimeri6@gmail.com>
2019-04-03 17:19:08 +0200

    Update app.ts

--- app/app.ts
+++ app/app.ts
@@ -140,9 +140,7 @@ app.route('/api/structograms/:id').get((req, res) => {
 });
 
 app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname, 'public/index.html'), () => {
-		res.sendFile(path.join(__dirname, 'static/error.html'));
-	});
+	res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 app.listen(conf.port, () => {
